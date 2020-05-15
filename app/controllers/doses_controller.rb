1556808ff96e7f78693cf495951cb3f8b63469f7
Hyejeong Kwon <hyejeong206@gmail.com>
2020-05-15 15:26:34 +0900

    middle

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,7 +20,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_parh(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
 
